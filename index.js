891d8fcdf04dd42dbc386cc5065eafc42f8f6841
Dilara Katuk <dilarakatuk@gmail.com>
2022-04-24 23:49:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun("swim"));
+
+console.log(mondayWork("work from home")); // hoisting
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+
+function wrapAdjective(adj="*"){
+    const x = function innerFun(par = "special") {
+        return `You are ${adj}${par}${adj}!`
+    }
+    return x;
+ };
+ console.log(wrapAdjective()("a dedicated programmer"));
